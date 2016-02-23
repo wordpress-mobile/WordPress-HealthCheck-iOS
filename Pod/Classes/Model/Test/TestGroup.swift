52015c1919433a6068a9961247bad99298bd50e6
Diego Rey Mendez <diegoreymendez@gmail.com>
2016-02-23 15:58:02 -0800

    Improves the test classes.

--- Pod/Classes/Model/Test/TestGroup.swift
+++ Pod/Classes/Model/Test/TestGroup.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 class TestGroup {
-    
+
     /// The result block for running all tests.
     ///
     /// - Parameters:
@@ -10,46 +10,58 @@ class TestGroup {
     ///         and localized description of the errors found in the tests run, and a proposed
     ///         solution on how to fix those errors.
     ///
-    //typealias Result = (success: Bool, errorDescription: String?) -> ()
-    
+    typealias GroupCompletionHandler = (success: Bool) -> ()
+
     /// The tests in this group.
     ///
-    let tests : [Test]
+    let tests: [Test]
     
-    /// The progress object for when tests are being run.
+    /// Default initializer
     ///
-    let progress = NSProgress()
-    
     init(tests : [Test]) {
         self.tests = tests
-        
-        for test in tests {
-            progress.addChild(test.progress(), withPendingUnitCount: 1)
-        }
     }
     
     // MARK: - Running & resetting
     
     /// Runs all the tests in this test group.
     ///
-    func runAll() {
+    /// - Parameters:
+    ///     - onTestComplete: will be executed once for every test in the group.
+    ///     - onGroupCmplete: will be executed on time, once all tests in the group have executed.
+    ///
+    func runAll(
+        onTestComplete: TestCompletionHandler,
+        onGroupComplete: GroupCompletionHandler) {
+        
+        let testsToRun = tests.count
+        var testsRun = 0
+        var anyTestFailed = false
+        
         for test in tests {
-            let result = {(test: Test, success: Bool) -> () in
-            }
-            
-            test.run(result)
-            
-            if test === test {
+            test.run({(test: Test, success: Bool) -> () in
                 
-            }
+                onTestComplete(test: test, success: success)
+                
+                if (!success) {
+                    anyTestFailed = true
+                }
+                
+                testsRun += 1
+                assert(testsRun <= testsToRun)
+                
+                if testsRun == testsToRun {
+                    onGroupComplete(success: !anyTestFailed)
+                }
+            })
         }
     }
     
-    /// Resets the state of all tests in this group.  Should be called before running the test again.
+    /// Resets the results of all tests in this group.
     ///
-    func resetAll() {
+    func resetResults() {
         for test in tests {
-            test.reset()
+            test.result = nil
         }
     }
 }
