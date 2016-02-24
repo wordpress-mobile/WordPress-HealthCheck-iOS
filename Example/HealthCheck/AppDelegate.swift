884db8d3035b97b38403cd14f7f3cd9ec2c489cd
Diego Rey Mendez <diegoreymendez@gmail.com>
2016-02-24 14:41:50 -0800

    Added the tests view controller.

--- Example/HealthCheck/AppDelegate.swift
+++ Example/HealthCheck/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  HealthCheck
-//
-//  Created by Sérgio Estêvão on 02/23/2016.
-//  Copyright (c) 2016 Sérgio Estêvão. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
