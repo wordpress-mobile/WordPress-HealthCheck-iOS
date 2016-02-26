63b5cc4daace9577cd522f157c554484817e0f27
Matt Bumgardner <matt@bumgardner.org>
2016-02-26 10:00:11 -0600

    Added detail VC

---
+++ Example/HealthCheck/TestDetailViewController.swift
@@ -0,0 +1,17 @@
+import Foundation
+import HealthCheck
+import UIKit
+
+class TestDetailViewController: UIViewController {
+    
+    var test: Test?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
