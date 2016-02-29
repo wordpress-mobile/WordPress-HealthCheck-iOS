0b460017c0e4642c3811f0e704664e9cf92d4dc0
Matt Bumgardner <matt@bumgardner.org>
2016-02-29 10:01:51 -0600

    Added a stackview

--- Example/HealthCheck/TestDetailViewController.swift
+++ Example/HealthCheck/TestDetailViewController.swift
@@ -3,8 +3,19 @@ import HealthCheck
 import UIKit
 
 class TestDetailViewController: UIViewController {
-    
+
     var test: Test?
+
+    @IBOutlet weak var TestNameLabel: UILabel!
+    @IBOutlet weak var TestNameDetail: UILabel!
+    @IBOutlet weak var TestDescriptionLabel: UILabel!
+    @IBOutlet weak var TestDescriptionDetail: UILabel!
+    @IBOutlet weak var ErrorReasonLabel: UILabel!
+    @IBOutlet weak var ErrorReasonDetail: UILabel!
+    @IBOutlet weak var ErrorProposedFixLabel: UILabel!
+    @IBOutlet weak var ErrorProposedFixDetail: UILabel!
+    @IBOutlet weak var ErrorTechnicalDetailsLabel: UILabel!
+    @IBOutlet weak var ErrorTechnicalDetailsDetail: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,5 +24,5 @@ class TestDetailViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
+
 }
